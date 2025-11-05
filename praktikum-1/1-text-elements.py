1b2067a9fe835f4ca6e0fa0ffaee4dba0fe41008
distiafajar28 <distiafajar28@gmail.com>
2025-11-05 11:03:46 +0700

    init praktikum 1 dan 2

---
+++ praktikum-1/1-text-elements.py
@@ -0,0 +1,44 @@
+import streamlit as st
+st.write("Hello")
+st.write('World!!!')
+
+st.title("This is our Title")
+st.header("""This is our Header""")
+st.subheader("""This is our Sub-Header""")
+st.caption("""This is our Caption""")
+
+st.text("Hi, \nPeople\t!!!!!!!!")
+st.text('Welcome to')
+st.text(""" Streamlit's World""")
+
+st.markdown("# Hi,\n# ***People*** \t !!!!!!!")
+st.markdown('## Welcome to')
+st.markdown(""" Streamlit's World""")
+
+st.latex(r'''cos2\theta = 1 - 2sin^\theta''')
+st.latex("""(a+b)^2 = a^2 + b^2 + 2ab""")
+st.latex(r'''\frac{\partial u}{\partial t}) = h^2 \left( /frac{/partial^2 u}{\partial x^2} + \frac{\partial^2 u}{\partial y^2}
+         + \frac{\partial^2 u}{\partial z^2} \right)''')
+
+st.subheader("""Python Code""")
+code = '''def hello() :
+print("Hello, Streamlit!")'''
+st.code(code,  language='python')
+
+st.subheader("""Java Code""")
+st.code("""public class GFG {
+        Public static void main(String args[])})
+        {
+        System.out.print1n("Hello World");
+        }
+        }""", language='javascript')
+st.subheader("""JavaScript Code""")
+st.code(""" <p id="demo"></p>
+        <script>
+        try {
+        adddlert("Welcome guest!");
+        }
+        catch(err) {
+        document.getElementById("demo").innerHTML = err.message;
+        }
+        </script> """)
---
+++ praktikum-1/2-data-elements.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd 
+import numpy as  np
+
+df = pd.DataFrame(
+  np.random.randn(30,10),
+  columns=('col_no %d' % i for i in range (10))) 
+st.dataframe(df)
+
+df = pd.DataFrame(
+  np.random.randn(30, 10),
+  columns=('col_no %d' % i for i in range(10)))
+st.dataframe(df.style.highlight_min(axis=0))
+
+df = pd.DataFrame(
+  np.random.randn(30, 10),
+  columns=('col_no %d' % i for i in range(10)))
+st.table(df)
+
+st.metric(label="Temperature", value="31 c",  delta="1.2 c")
+c1, c2, c3 = st.columns(3)
+c1.metric("Rainfall", "100 cm", "10 cm")
+c2.metric(label="Population", value="123 Bilions", delta="1 Billions", delta_color="inverse")
+c3.metric(label="Customers", value=100, delta=10, delta_color="off")
+st.metric(label="Speed", value=None, delta=0)
+st.metric("Trees", "91456", "-1132649")
+
+df = pd.DataFrame(
+  np.random.randn(30, 10),
+  columns=('col_no %d' % i for i in range(10)))
+st.write('Here is our Data', df, 'Data is in dataframe format. \n', "\nWrite is Super funcition")
+
+df = pd.DataFrame(
+  np.random.randn(10, 2),
+  columns=['a', 'b'])
+
+import altair as alt
+chart = alt.Chart(df).mark_bar().encode(
+  x='a', y='b', tooltip=['a', 'b'])   
+st.write(chart)
+
+"Adding 5 & 4 =", 5+4
+a = 5
+'a', a
+"""
+# Magic Feature
+Markdown working without defining its function explicitly."""
+df = pd.DataFrame({'col': [1,2]})
+'dataframe', df 
+
+import matplotlib.pyplot as plt
+import numpy as np
+s = np.random.logistic(10, 5, size=5)
+chart, ax = plt.subplots()
+ax.hist(s, bins=15)
+"chart", chart
